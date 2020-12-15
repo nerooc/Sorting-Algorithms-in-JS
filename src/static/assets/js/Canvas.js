b710564dfbfa8f628ba08918fffb51ac8c282369
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-12-15 18:12:09 +0100

    docs(): added comments in the Canvas.js file

--- src/static/assets/js/Canvas.js
+++ src/static/assets/js/Canvas.js
@@ -39,41 +39,54 @@ waitForElementToDisplay(
     ////////////////////////////////////////////////////////////////////////////////////////////////
 
     // Funkcja generująca bloki do sortowania
-    function generateBlocks(amount = 20) {
-      if (amount && typeof amount !== 'number') {
-        alert('First argument must be a typeof Number');
+    function generateBlocks(num = 20) {
+      if (num && typeof num !== 'number') {
+        alert('First argument must be a Number');
         return;
       }
-      for (let i = 0; i < amount; i += 1) {
+
+      for (let i = 0; i < num; i += 1) {
+        // Losujemy wartość bloku
         const value = Math.floor(Math.random() * 100);
+
+        // Tworzymy blok
         const block = document.createElement('div');
+
+        // Nadajemy mu klasę
         block.classList.add('block');
 
+        // Ustawiamy go w odpowiednim miejscu i dajemy odpowiednią wysokość
         block.style.height = `${value * 2}px`;
         block.style.left = '2%';
         block.style.transform = `translateX(${i * 120}%)`;
 
+        // Tworzymy opis bloku
         const blockLabel = document.createElement('label');
         blockLabel.classList.add('block__id');
         blockLabel.innerHTML = value;
 
+        // Dodajemy opis do bloku i blok do przeznaczonego elementu canvas
         block.appendChild(blockLabel);
         canvas.appendChild(block);
       }
     }
 
+    // Funkcja zamieniająca dwa pliki miejscami
     function swap(el1, el2) {
       return new Promise((resolve) => {
+        // Kopiujemy style obu elementów
         const style1 = window.getComputedStyle(el1);
         const style2 = window.getComputedStyle(el2);
 
+        // Bierzemy pozycje z własciwości transform
         const transform1 = style1.getPropertyValue('transform');
         const transform2 = style2.getPropertyValue('transform');
 
+        // Zamieniamy pozycjami
         el1.style.transform = transform2;
         el2.style.transform = transform1;
 
-        // Wait for the transition to end!
+        // Razem z animacją zamieniamy elementy miejscami w kodzie HTML
         window.requestAnimationFrame(function () {
           setTimeout(() => {
             canvas.insertBefore(el2, el1);
@@ -83,44 +96,58 @@ waitForElementToDisplay(
       });
     }
 
+    // Funkcja przedstawiająca sortowanie bąbelkowe na naszych elementach
     async function bubbleSort() {
+      // NodeList stworzony z naszych bloków
       let blocks = document.querySelectorAll('.block');
+
+      // Sortowanie
       for (let i = 0; i < blocks.length - 1; i += 1) {
         for (let j = 0; j < blocks.length - i - 1; j += 1) {
-          blocks[j].style.backgroundColor = '#FF4949';
+          // Zamiana kolorów elementów które porównujemy
+          blocks[j].style.backgroundColor = 'red';
           blocks[j + 1].style.backgroundColor = '#FF4949';
 
+          // Opóźnienie
           await new Promise((resolve) =>
             setTimeout(() => {
               resolve();
             }, 500)
           );
 
+          // Wyciągnięcie wylosowanych wcześniej wartości z porównywanych bloków
           const value1 = Number(blocks[j].childNodes[0].innerHTML);
           const value2 = Number(blocks[j + 1].childNodes[0].innerHTML);
 
+          // Porównanie
           if (value1 > value2) {
+            // Zamiana
             await swap(blocks[j], blocks[j + 1]);
+            // Ponowne wybranie bloków, tym razem razem ze zmianami
             blocks = document.querySelectorAll('.block');
           }
 
+          // Elementy wracają do początkowych kolorów
           blocks[j].style.backgroundColor = '#58B7FF';
           blocks[j + 1].style.backgroundColor = '#58B7FF';
         }
 
-        blocks[blocks.length - i - 1].style.backgroundColor = '#13CE66';
+        // Element zatwierdzony (ostatni) dostaje zielony kolor
+        blocks[blocks.length - i - 1].style.backgroundColor = 'green';
       }
     }
 
+    // Od razu generujemy bloki
     generateBlocks();
 
+    // Po wciśnięciu przycisku start, włączamy sortowanie
     startButton.addEventListener('click', () => {
       bubbleSort();
       startButton.disabled = true;
     });
 
+    // Po wcisnięciu przycisku reload, tworzymy nowy zestaw bloków
     reloadButton.addEventListener('click', () => {
-      // Empty the canvas
       startButton.disabled = false;
       canvas.innerHTML = '';
       generateBlocks(20);
@@ -140,7 +167,7 @@ function waitForElementToDisplay(
 ) {
   let startTimeInMs = Date.now();
 
-  // IIFE które przechodzi po drzewie w poszukiwaniu naszego elementu
+  // IIFE które poszukuje nasz element
   (function loopSearch() {
     if (document.querySelector(selector) != null) {
       callback();
